f5332d62fb9701e8df4884d85d6f01cd56222b3d
Ratheesh <ratheesh.ramadasan@gmail.com>
2024-04-16 21:02:40 -0600

    New Project

---
+++ OutlookScript.ps1
@@ -0,0 +1,17 @@
+﻿
+if (Get-Process | Where-Object name -contains outlook) {
+            Write-Output 'Outlook already running.'
+            $outlook = New-Object -COM Outlook.Application;
+            $mapi = $outlook.GetNameSpace("MAPI");
+            $mapi.logon()
+    }
+else {
+            Write-Output 'Starting Outlook application...'
+		$exe = (Get-ItemProperty -Path Outlook.exe).'(default)'
+            Get-Process Start-Process "Outlook" '(default)'
+            Write-Output 'Outlook Reconnecting'
+            $outlook = New-Object -COM Outlook.Application;
+            $mapi = $outlook.GetNameSpace("MAPI");
+            $mapi.logon()
+    }
+
